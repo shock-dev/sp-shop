49def576205fd51d7c90b23d90a60a3f39d9ebe2
shock-dev <shock.work@mail.ru>
2023-05-07 12:38:52 +0300

    back typing

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -1,6 +1,5 @@
-export const arrayUniqueByKey = (
-  items: { [k: string]: any }[],
-  key: string,
-) => [...new Map(items.map(i => [i[key], i])).values()];
+export const arrayUniqueByKey = (items: any[], key: string) => [
+  ...new Map(items.map(i => [i[key], i])).values(),
+];
 
 export const numWithSpaces = (n: number) => n.toLocaleString();
